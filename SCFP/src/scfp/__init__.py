a58a6a156cbbe7de129451089310e2efb161ae31
gr7-cipher <gaith_rj@yahoo.com>
2026-03-31 17:47:53 -0400

    Add files via upload

---
+++ SCFP/src/scfp/__init__.py
@@ -0,0 +1,54 @@
+"""
+Self-Correction Failure Prediction (SCFP) Framework
+
+A comprehensive framework for predicting when and how Large Language Models
+will fail during intrinsic self-correction processes.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Shahed Almobydeen, Gaith Rjoub, Jamal Bentahar, Ahmad Irjoob"
+__email__ = "salmobydeen@aut.edu.jo"
+
+from .models import (
+    DeBERTaFailurePredictor,
+    BaselineModels,
+    FailureTaxonomy
+)
+
+from .data import (
+    SCFPDataset,
+    CorrectionTrace
+)
+
+from .training import (
+    Trainer,
+    TrainingConfig,
+    EvaluationMetrics
+)
+
+from .routing import (
+    DynamicRouter,
+    CostModel,
+    RoutingStrategy
+)
+
+__all__ = [
+    # Models
+    "DeBERTaFailurePredictor",
+    "BaselineModels", 
+    "FailureTaxonomy",
+    
+    # Data
+    "SCFPDataset",
+    "CorrectionTrace",
+    
+    # Training
+    "Trainer",
+    "TrainingConfig",
+    "EvaluationMetrics",
+    
+    # Routing
+    "DynamicRouter",
+    "CostModel",
+    "RoutingStrategy",
+]
