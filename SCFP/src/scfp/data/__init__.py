5083ee18f42c7918e7b5d1c93b4a2701436dfb9e
gr7-cipher <gaith_rj@yahoo.com>
2026-03-31 17:49:30 -0400

    Add files via upload

--- SCFP/src/scfp/data/__init__.py
+++ SCFP/src/scfp/data/__init__.py
@@ -3,14 +3,12 @@ Data loading and preprocessing utilities for SCFP framework.
 """
 
 from .dataset import SCFPDataset, CorrectionTrace
-from .synthetic import SyntheticDataGenerator
 from .preprocessing import DataPreprocessor
 from .loaders import create_dataloaders
 
 __all__ = [
     "SCFPDataset",
     "CorrectionTrace", 
-    "SyntheticDataGenerator",
     "DataPreprocessor",
     "create_dataloaders",
 ]
